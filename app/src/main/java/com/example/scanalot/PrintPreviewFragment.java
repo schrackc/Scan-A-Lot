e9d4dd7265dbcd6b37f62edfb6a081c51566685f
Hoffera1 <andrew.hoffer@stvincent.edu>
2023-03-13 23:12:34 -0400

    preview Print

--- app/src/main/java/com/example/scanalot/PrintPreviewFragment.java
+++ app/src/main/java/com/example/scanalot/PrintPreviewFragment.java
@@ -36,13 +36,21 @@ public class PrintPreviewFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String strMonth = LocalDateTime.now().getMonth().toString() + "/";
+            int iDay = LocalDateTime.now().getDayOfMonth();
+            int iYear = LocalDateTime.now().getYear();
 
-            binding.textViewViolationTime.setText(binding.textViewViolationTime.getText() + LocalDateTime.now().toString());
-            binding.textView2.setText(binding.textView2.getText() + viewModel.getParkingLot().getValue());
+            binding.textViewViolationTime.setText(binding.textViewViolationTime.getText() + strMonth + iDay + "/" + iYear);
+
+            binding.textViewCitationLocation.setText(binding.textViewCitationLocation.getText() + viewModel.getParkingLot().getValue());
+
             binding.textViewFineAmount.setText(binding.textViewFineAmount.getText() + " $100.00");
+
             binding.textViewLicensePlate.setText(binding.textViewLicensePlate.getText() + viewModel.getLicenseNumber().getValue());
-            binding.textViewVehicleColor.setText(binding.textViewVehicleColor.getText() + viewModel.getVehicleList().getValue().get(viewModel.getReferenceNum()).getModel().toString());
-            binding.textViewVehicleModel.setText(binding.textViewVehicleModel.getText() + viewModel.getVehicleList().getValue().get(viewModel.getReferenceNum()).getColor().toString());
+
+            binding.textViewVehicleColor.setText(binding.textViewVehicleColor.getText() + viewModel.getVehicleList().getValue().get(viewModel.getReferenceNum()).getColor().toString());
+
+            binding.textViewVehicleModel.setText(binding.textViewVehicleModel.getText() + viewModel.getVehicleList().getValue().get(viewModel.getReferenceNum()).getModel().toString());
         }
     }
 
